eefcd37010cd7cd4b412142525f4c9f201857151
wojtek <wojtek.klusek2@gmail.com>
2018-04-20 18:58:07 +0100

    index fix

--- index.php
+++ index.php
@@ -1,12 +1,6 @@
 <?php
 
 include("/templ_scripts/before_content_template.php");
-include "/templ_scripts/connect.php";
-
-$book_rank = "SELECT id,
-              RANK() OVER (ORDER BY id) AS ROW_NUMBER
-              FROM copy;"
-
 echo '
 
 <!--CONTENT-->
@@ -18,19 +12,11 @@ echo '
             <li><p>Gdy już je założysz, zaloguj się, przejdź do Katalogu Online by przejrzeć książki w bibliotece.</p></li>
             <li><p>Możesz też sprawdzić wypożyczone książki klikając na nazwę swojego użytkownika po prawej stronie górnego menu.</p></li>
         </ul>
-';
-
-echo '
-
-
-
     </div>
 </div><!--/row-->
-
 </div><!--/span-->
 
 ';
 
 include("/templ_scripts/after_content_template.php");
-mysql_close($id_conn);
 ?>
